1ca8c6cd1aaf458135f2c66efacb42194b1d01d7
Todd Meinershagen <todd@meinershagen.net>
2016-02-16 08:10:21 -0600

    Adding the first demo project.

---
+++ src/Demo.Budgeting/Demo.Budgeting.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace Demo.Budgeting.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/bootstrap")
+                .Include("~/Content/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular")
+                .Include("~/Scripts/angular.min.js")
+                .IncludeDirectory("~/app", "*.js")
+                );
+        }
+    }
+}
---
+++ src/Demo.Budgeting/Demo.Budgeting.Web/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Optimization;
+
+using Demo.Budgeting.Web.App_Start;
+
+namespace Demo.Budgeting.Web
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
